7cd9f37af5d008faec528b83f6f54ef33b176b02
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-06-14 14:13:30 +0300

    addition, subtraction and multiplication

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+num1=10
+num2=5
+
+print("addition of", num1, "and", num2, "is:", (num1 + num2))
+print("subtraction of", num1, "and", num2, "is:", (num1 - num2))    
+print("multiplication of", num1, "and", num2, "is:", (num1 * num2))         
