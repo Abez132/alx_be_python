15077c484c6d2e49331ad0e49acd5250ee68f876
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-06-14 14:26:47 +0300

    num

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
-num1 = 10
-num2 = 5
+number1 = 10
+number2 = 5
 
-print("addition of", num1, "and", num2, "is:", (num1 + num2))
-print("subtraction of", num1, "and", num2, "is:", (num1 - num2))    
-print("multiplication of", num1, "and", num2, "is:", (num1 * num2))         
+print("addition of", number1, "and", number2, "is:", (number1 + number2))
+print("subtraction of", number1, "and", number2, "is:", (number1 - number2))    
+print("multiplication of", number1, "and", number2, "is:", (number1 * number2))         
